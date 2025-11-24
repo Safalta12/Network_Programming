7d2a6f49d7f338033532db3a8c171a72d36c2b18
Safalta Shrestha <141534902+Safalta12@users.noreply.github.com>
2025-11-24 20:26:20 +0545

    Add files via upload
    Codes of chapter-5(URL Connection)

---
+++ chapter5/ConfiguringConnection.java
@@ -0,0 +1,28 @@
+package chapter5;
+
+import java.io.IOException;
+import java.net.*;
+public class ConfiguringConnection {
+public static void main(String[] args) {
+	try {
+		URL url=new URL("https://www.example.com");
+		URLConnection connection= url.openConnection();
+		System.out.println("URL:" + connection.getURL());
+		System.out.println("Allow User Interaction:" + connection.getAllowUserInteraction());
+		System.out.println("Do Output:" + connection.getDoOutput());
+		System.out.println("Use Cache:" + connection.getUseCaches() +"\n");
+		
+		connection.setAllowUserInteraction(true);
+		connection.setDoInput(true);
+		connection.setUseCaches(false);
+		
+		System.out.println("After Configuration \n");
+		System.out.println("Allow User Interaction:" + connection.getAllowUserInteraction());
+		System.out.println("Do Output:" + connection.getDoOutput());
+		System.out.println("Use Cache:" + connection.getUseCaches() +"\n");
+		connection.connect();
+	}catch(IOException e) {
+		e.printStackTrace();
+	}
+}
+}
